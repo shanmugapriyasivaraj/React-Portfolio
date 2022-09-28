b19ca44840a5884e25260ce54c541f9233c3fcbf
shanmugapriyasivaraj <mpriyasivaraj28@gmail.com>
2022-09-28 08:02:02 +0530

    commit

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import {SidebarContainer,Icon,CloseIcon,SidebarWrapper,SidebarMenu,SidebarLink} from './SidebarElements'
-function Sidebar({isOpen,toggle}) {
-    return (
-        <SidebarContainer isOpen={isOpen} onClick={toggle}>
-            <Icon onClick={toggle}> 
-                <CloseIcon />
-            </Icon>
-            <SidebarWrapper>
-                <SidebarMenu>
-                    <SidebarLink to='home' onClick={toggle}>
-                        Home
-                    </SidebarLink>
-                    <SidebarLink to='about' onClick={toggle}>
-                        About
-                    </SidebarLink>
-                    <SidebarLink to='skills' onClick={toggle}>
-                        Skills
-                    </SidebarLink>
-                    <SidebarLink to='portfolio' onClick={toggle}>
-                        Portfolio
-                    </SidebarLink>
-                    <SidebarLink to='contact' onClick={toggle}>
-                        Contact
-                    </SidebarLink>
-                </SidebarMenu>
-            </SidebarWrapper>
-        </SidebarContainer>
-    )
+import React from "react";
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+} from "./SidebarElements";
+function Sidebar({ isOpen, toggle }) {
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="" onClick={toggle}>
+            Home
+          </SidebarLink>
+          <SidebarLink to="" onClick={toggle}>
+            About
+          </SidebarLink>
+        </SidebarMenu>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
